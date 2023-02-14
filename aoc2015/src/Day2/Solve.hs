79b22207ee56f2c36c4a09bce63b710875f47085
falcowinkler <8613031+falcowinkler@users.noreply.github.com>
2023-02-14 19:01:44 +0100

    Implement solution for 2015 day 2

--- aoc2015/src/Day2/Solve.hs
+++ aoc2015/src/Day2/Solve.hs
@@ -1,24 +1,42 @@
 module Day2.Solve where
 import Text.Parsec
+import Data.List (sort)
 
 giftSizes = endBy line eol
-line = do
-  length <- cell
-  char ('x')
-  width <- cell
-  char ('x')
-  height <- cell
-  return $ Dimensions { l = length, w = width, h = height }
+line = cell >>= \parcelLength ->
+       char 'x' *>
+       cell >>= \parcelWidth ->
+       char 'x' *>
+       cell >>= \parcelHeight ->
+       return Dimensions { l = parcelLength, w = parcelWidth, h = parcelHeight }
 cell = read <$> many digit
 eol = char '\n'
 
 data Dimensions = Dimensions { l :: Int, w :: Int, h :: Int } deriving (Show, Eq)
 
-parseGiftSizes :: String -> Either ParseError [Dimensions]
-parseGiftSizes input = parse giftSizes "(unknown)" input
+parseGiftSizes :: String -> [Dimensions]
+parseGiftSizes input = fatalParseError $ parse giftSizes "(unknown)" input
+  where
+    fatalParseError (Left _) = error "Invalid input"
+    fatalParseError (Right result) = result
+
+calculateWrappingPaper :: Dimensions -> Int
+calculateWrappingPaper d = (2 * a) + (2 * b) + (2 * c) + minimum [a, b, c]
+  where
+    a = l d * w d
+    b = w d * h d
+    c = h d * l d
 
 solve1 :: String -> Int
-solve1 _ = 1
+solve1 = sum . map calculateWrappingPaper . parseGiftSizes
+
+calculateRibbonLength :: Dimensions -> Int
+calculateRibbonLength d = 2 * x + 2 * y + a * b * c
+  where
+    a = l d
+    b = w d
+    c = h d
+    (x:y:_) = sort [a, b, c]
 
 solve2 :: String -> Int
-solve2 _ = 1
+solve2 = sum . map calculateRibbonLength . parseGiftSizes
--- aoc2015/test/Day2/SolveSpec.hs
+++ aoc2015/test/Day2/SolveSpec.hs
@@ -4,17 +4,22 @@ module Day2.SolveSpec where
 import Test.Hspec
 import System.IO
 import Day2.Solve
+import GHC.Real (FractionalExponentBase(Base10))
 
 spec :: Spec
 spec = do
-  describe "ParseGiftSizes" $do
-    it "parses correctly" $ do
-      parseGiftSizes "12x4x13\n" `shouldBe` (Right $ [Dimensions { l = 12, w = 4, h = 13 }])
-  describe "Calculate the correct floor for santa to arrive in" $ do
-    referenceInput <- runIO $ openFile "test/Day1/testinput.txt" ReadMode >>= hGetContents
+  describe "Calculate needed wrapping paper correctly" $ do
+    referenceInput <- runIO $ openFile "test/Day2/testinput.txt" ReadMode >>= hGetContents
     it "works for reference input" $ do
-      solve1 referenceInput `shouldBe` 74
-  describe "Calculates the instruction position that causes santa to enter the basement" $ do
-    referenceInput <- runIO $ openFile "test/Day1/testinput.txt" ReadMode >>= hGetContents
+      solve1 referenceInput `shouldBe` 1598415
+    it "works for examples" $ do
+      solve1 "2x3x4\n" `shouldBe` 58
+      solve1 "1x1x10\n" `shouldBe` 43
+      solve1 "2x3x4\n1x1x10\n" `shouldBe` 58 + 43
+  describe "Calculates needed ribbon" $ do
+    referenceInput <- runIO $ openFile "test/Day2/testinput.txt" ReadMode >>= hGetContents
     it "works for reference input" $ do
-      solve2 referenceInput `shouldBe` 74
+      solve2 referenceInput `shouldBe` 3812909
+    it "works for examples" $ do
+      solve2 "2x3x4\n" `shouldBe` 34
+      solve2 "1x1x10\n" `shouldBe` 14
