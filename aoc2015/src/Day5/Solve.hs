6d71b74ba50830466d08c2f99d17c4c425f3ebde
falcowinkler <8613031+falcowinkler@users.noreply.github.com>
2023-02-28 21:25:14 +0200

    2015 day 5 (2)

--- aoc2015/src/Day5/Solve.hs
+++ aoc2015/src/Day5/Solve.hs
@@ -1,18 +1,28 @@
 module Day5.Solve (solve1, solve2) where
-import Data.List.Split
-import Data.List (isInfixOf)
+import Data.List.Split ( splitOn )
+import Data.List ( isInfixOf )
 
-nice :: String -> Bool
-nice s = hasVowels && hasEqualAdjacentPair && noNaughtyPairs
+nice1 :: String -> Bool
+nice1 s = hasVowels && hasEqualAdjacentPair && noNaughtyPairs
   where
     hasVowels = length (filter (`elem` "aeiou") s) >= 3
     hasEqualAdjacentPair = or [x == y | (x, y) <- zip s (tail s)]
     noNaughtyPairs = not $ or [naughtyPair `isInfixOf` s | naughtyPair <- ["ab", "cd", "pq", "xy"]]
 
-solve1 :: String -> Int
-solve1 input = sum $ map (fromEnum . nice) strings
+nice2 :: String -> Bool
+nice2 s = rule1 s && rule2
+  where
+    rule1 (x:y:xs) = isInfixOf [x,y] xs || rule1 (y:xs)
+    rule1 _ = False
+    rule2 = or [x == y | (x, _, y) <- zip3 s (tail s) (tail (tail s))]
+
+sumNiceLines :: (String -> Bool) -> String -> Int
+sumNiceLines rule input = sum $ map (fromEnum . rule) strings
   where
     strings = splitOn "\n" input
 
+solve1 :: String -> Int
+solve1 = sumNiceLines nice1
+
 solve2 :: String -> Int
-solve2 _ = 0
+solve2 = sumNiceLines nice2
