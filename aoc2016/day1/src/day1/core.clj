64aa096a9719d3666ac4f3fa783940d86d2aa78b
Falco Winkler <8613031+falcowinkler@users.noreply.github.com>
2024-07-28 17:55:44 +0200

    day1 2016

--- aoc2016/day1/src/day1/core.clj
+++ aoc2016/day1/src/day1/core.clj
@@ -1,10 +1,10 @@
 (ns day1.core
-  (:require [clojure.java.io :as io])
-  (:require [clojure.string :as str])
+  (:require [clojure.java.io :as io]
+            [clojure.string :as str])
   (:gen-class))
 
-(def data-file (io/resource
-                "input.txt"))
+(defn load-input [] (slurp (io/resource
+                            "input.txt")))
 
 (def initial-state {:direction [0 -1] :location [0 0]})
 
@@ -17,22 +17,55 @@
         [dx dy] (state :direction)
         [x y] (state :location)
         [nx ny] (if (= \L dir) [dy (- dx)] [(- dy) dx])]
-    {:direction [nx ny] :location [(+ x (* nx step)) (+ y (* ny step))]
-     }
-    ))
+    {:direction [nx ny] :location [(+ x (* nx step)) (+ y (* ny step))]}))
 
-(defn follow-instructions [instructions]
-  (let [tokens (str/split instructions #",\s*")]
-    (reduce update-state initial-state tokens)
-    ))
+(defn follow-instructions [tokens]
+  (reduce update-state initial-state tokens))
+
+(defn tokenize [input]
+  (str/split input #",\s*"))
 
 (defn distance [state]
   (taxicab (state :location)))
 
-(defn solve []
-  (distance (follow-instructions (slurp data-file))))
+(defn solve1 [input]
+  (distance (follow-instructions (tokenize input))))
+
+(defn max [& args]
+  (loop [max nil
+         seq args]
+    (if (empty? seq) max
+        (let [n (first seq)]
+          (if (or (nil? max) (< max n)) (recur n (rest seq)) (recur max (rest seq)))))))
+
+(defn add-five [x] (+ x 5))
+
+;(= __ (add-five 3))
+
+(defn reverse-interleave [col n]
+  (for [start (range n)]
+    ((defn every-nth [col n start]
+       (for [i (range start (count col) n)]
+         (nth col i))) col n start)))
+
+(defn calc-intersection' [[x1 y1] [x2 y2]])
+
+(defn calc-intersection [[head & tail]]
+  nil ; todo, return coordinates of overlap if there is an overlap with head, with any of the tail
+  )
+
+(defn solve2 [input]
+  (let [states (reductions update-state initial-state (tokenize input))
+        visited-positions (map :location states)
+        pairs (reverse (partition 2 1 visited-positions))]
+    (loop [list pairs]
+      (if (empty? list) nil
+          (let [intersection (calc-intersection list)]
+            (if (nil? intersection)
+              (recur (rest list))
+              (intersection)))))))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (println (solve1 (load-input))))
--- aoc2016/day1/test/day1/core_test.clj
+++ aoc2016/day1/test/day1/core_test.clj
@@ -2,6 +2,17 @@
   (:require [clojure.test :refer :all]
             [day1.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest test-update-state
+  (testing "state is updated correctly"
+    (is (= (update-state initial-state "L3")
+           {:direction [(- 1) 0] :location [(- 3) 0]}))
+    (is (= (update-state {:direction [1 0] :location [2 0]} "L3")
+           {:direction [0 (- 1)] :location [2 (- 3)]}))
+    (is (= (update-state {:direction [1 0] :location [5 4]} "R3")
+           {:direction [0 1] :location [5 7]}))))
+
+(deftest test-solve
+  (testing "if solve function solves example inputs correctly"
+    (is (= (solve1 "R2, L3") 5))
+    (is (= (solve1 "R2, R2, R2") 2))
+    (is (= (solve1 "R5, L5, R5, R3") 12))))
